6ee3af148e67d74026b2fe28c1f709ae84fa257c
3CSTVZY <corp.of.alexeev@gmail.com>
2022-12-04 16:09:21 +0200

    LC_02

---
+++ LC_02/intro.py
@@ -0,0 +1,13 @@
+# with open('file.txt', 'w') as data:
+#   data.write('\nLine2\n')
+#   data.write('Line3\n')
+
+
+# path = 'file.txt'
+# data = open(path, 'r')
+# for line in data:
+#   print(line)
+# data.close()
+
+import hello as h
+print(h.f(1))
